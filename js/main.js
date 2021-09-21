4f3ccac657b828e7f82e38e7424dbcfd60cf1c6c
piaocreative <johnpiao171019@gmail.com>
2021-09-22 06:46:56 +0800

    demo76-0922

---
+++ js/main.js
@@ -0,0 +1,107 @@
+$( document ).ready( function() {
+
+    if ( $( '.products' ).length ) {
+        $( '.products' ).slick({
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+    }
+
+    if ( $( '.logos' ).length ) {
+        $( '.logos' ).slick({
+            infinite: true,
+            slidesToShow: 7,
+            slidesToScroll: 2,
+            dots: false,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
+        });
+    }
+
+    if ( $( '.banners' ).length ) {
+        $('.banners').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            arrows:false,
+        });
+    }
+
+    if ( $( '.histories' ).length ) {
+        $('.histories').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            arrows:false,
+        });
+    }
+
+    $(".history-prev").click(function(e) {
+        e.preventDefault();
+        $('.histories').slick('slickPrev');
+    })
+      
+    $(".history-next").click(function(e) {
+        e.preventDefault();
+        $('.histories').slick('slickNext');
+    })
+
+});
